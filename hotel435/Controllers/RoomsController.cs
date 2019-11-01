ec6f30e8e488eb7f222b4401bbdb64ee7c2a6918
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2019-11-01 18:27:49 -0400

    Revert "Revert "Configured controllers to handle basic CRUD operations""
    This reverts commit fb8ed7b1179f2c153dab79fea0757a28d7b4e8f7.

--- hotel435/Controllers/RoomsController.cs
+++ hotel435/Controllers/RoomsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using hotel435.Models;
 using hotel435.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,34 +22,38 @@ namespace hotel435.Controllers
 
         // GET: api/Rooms
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<List<Room>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await _service.GetAllAsync();
         }
 
         // GET: api/Rooms/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public async Task<Room> GetByIdAsync(string id)
         {
-            return "value";
+            return await _service.GetByIdAsync(id);
         }
 
+        //NOTE: Current requirements do not mandate an Insert method so will be left alone for now
         // POST: api/Rooms
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
 
+        //}
+
+        //NOTE: Current requirements do not mandate an Update method so will be left alone for now
         // PUT: api/Rooms/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
+        //NOTE: Current requirements do not mandate a Delete method so will be left alone for now
         // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
