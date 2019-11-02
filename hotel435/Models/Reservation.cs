bb19579c24e0dccd3325cba3a6a0765f984db5af
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2019-11-02 15:33:03 -0400

    Update Reservation.cs
    Added extra models

--- hotel435/Models/Reservation.cs
+++ hotel435/Models/Reservation.cs
@@ -11,6 +11,8 @@ namespace hotel435.Models
         [Required]
         public string Id { get; set; }
         [Required]
+        public long ConfirmationNumber { get; set; }
+        [Required]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
@@ -20,7 +22,21 @@ namespace hotel435.Models
         public DateTime CheckIn { get; set; }
         [Required]
         public DateTime CheckOut { get; set; }
+        [Required] public Decimal Price { get; set; }
         [Required]
         public string RoomId { get; set; }
+        public string CreditCardNum { get; set; }
+        public string ExpMonth { get; set; }
+        public string ExpYear { get; set; }
+        public string Cvv { get; set; }
+        //Address represents street name
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Zip { get; set; }
+        [Required]
+        public string State { get; set; }
     }
 }
