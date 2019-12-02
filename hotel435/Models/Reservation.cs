01b472ce03ba019adc5c8f26df22c1c9ee2c0d03
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2019-12-01 20:45:07 -0500

    Update Reservation.cs

--- hotel435/Models/Reservation.cs
+++ hotel435/Models/Reservation.cs
@@ -14,9 +14,14 @@ namespace hotel435.Models
         public string Id { get; set; }
         public string ConfirmationNumber { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z\s-]*$",
+            ErrorMessage = "First Name is required and should only contain letters or a hyphen")]
         public string FirstName { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z\s-]*$",
+            ErrorMessage = "Last Name is required and should only contain letters or a hyphen")]
         public string LastName { get; set; }
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
@@ -25,12 +30,21 @@ namespace hotel435.Models
         public DateTime CheckOut { get; set; }
         public DateTime? ActualCheckIn { get; set; } = null;
         public DateTime? ActualCheckOut { get; set; } = null;
-        [Required] public Decimal Price { get; set; }
+        [Required] 
+        public Decimal Price { get; set; }
         [Required]
         public string RoomId { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{10,16}$",
+            ErrorMessage = "Credit Card Numbers must be between 10-16 digits")]
         public string CreditCardNum { get; set; }
+        [Required]
         public string ExpMonth { get; set; }
+        [Required]
         public string ExpYear { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{3}$",
+            ErrorMessage = "CVV must be 3 digits long")]
         public string Cvv { get; set; }
         //Address represents street name
         [Required]
@@ -38,6 +52,8 @@ namespace hotel435.Models
         [Required]
         public string City { get; set; }
         [Required]
+        [RegularExpression(@"^\d+$",
+            ErrorMessage = "Valid zip code required")]
         public string Zip { get; set; }
         [Required]
         public string State { get; set; }
